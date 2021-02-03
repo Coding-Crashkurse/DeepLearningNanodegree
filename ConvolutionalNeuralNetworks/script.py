ed5965b6a4a4d1f3dc4bb14194100ae0b092c938
markus <datamastery87@gmail.com>
2021-02-03 20:09:09 +0100

    first cnn prep

--- ConvolutionalNeuralNetworks/script.py
+++ ConvolutionalNeuralNetworks/script.py
@@ -1,3 +1,48 @@
-import torch.nn as nn
-import 
+import torch 
+import numpy as np
+
+# Load and visualize data
+
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+num_workers = 0
+batch_size = 20
+
+transform = transforms.ToTensor()
+
+train_data = datasets.MNIST(root="data", train=True, download=True, transform=transform)
+test_data = datasets.MNIST(root="data", train=False, download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
+
+import matplotlib.pyplot as plt
+
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy()
+
+
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx + 1, xticks=[], yticks=[])
+    ax.imshow(np.squeeze(images[idx]), cmap="gray")
+    ax.set_title(str(labels[idx].item()))
+    
+    
+img = np.squeeze(images[1])
+
+fig = plt.figure(figsize=(12, 12))
+ax = fig.add_subplot(111)
+ax.imshow(img, cmap="gray")
+width, height = img.shape
+thresh = img.max() / 2.5
+for x in range(width):
+    for y in range(height):
+        val = round(img[x][y], 2) if img[x][y] != 0 else 0
+        ax.annotate(str(val), xy=(y,x),
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    color="white" if img[x][y] < thresh else "black")
 
